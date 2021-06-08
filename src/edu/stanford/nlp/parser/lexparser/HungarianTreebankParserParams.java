2ca3feef0cbaebc6c227a42e3b6dc3bf44076351
John Bauer <horatio@gmail.com>
2021-06-07 23:13:10 -0700

    Implement a TreeNormalizer which completely strips the subcategories when reading the SPMRL treebank

--- src/edu/stanford/nlp/parser/lexparser/HungarianTreebankParserParams.java
+++ src/edu/stanford/nlp/parser/lexparser/HungarianTreebankParserParams.java
@@ -1,7 +1,9 @@
 package edu.stanford.nlp.parser.lexparser;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
 
 import edu.stanford.nlp.ling.HasWord;
 import edu.stanford.nlp.ling.Word;
@@ -11,15 +13,17 @@ import edu.stanford.nlp.trees.LeftHeadFinder;
 import edu.stanford.nlp.trees.MemoryTreebank;
 import edu.stanford.nlp.trees.PennTreeReaderFactory;
 import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeNormalizer;
 import edu.stanford.nlp.trees.TreeReaderFactory;
 import edu.stanford.nlp.trees.TreeTransformer;
 import edu.stanford.nlp.trees.international.hungarian.HungarianTreebankLanguagePack;
+import edu.stanford.nlp.util.StringUtils;
 import edu.stanford.nlp.util.logging.Redwood;
 
 /**
  * Bare-bones implementation of a ParserParams for the Hungarian SPMRL treebank.
  * <br>
- * Suitable for use in the SR Parser.  May need additional work to function in the PCFG.
+ * Suitable for use in the SR Parser.  Will need additional work to function in the PCFG.
  * Also, would likely function better with a new headfinder.
  */
 public class HungarianTreebankParserParams extends AbstractTreebankParserParams  {
@@ -33,6 +37,8 @@ public class HungarianTreebankParserParams extends AbstractTreebankParserParams 
   }
 
   private HeadFinder headFinder;
+
+  private TreeNormalizer normalizer = null;
 
   static final String[] EMPTY_SISTERS = new String[0];
 
@@ -81,20 +87,44 @@ public class HungarianTreebankParserParams extends AbstractTreebankParserParams 
 
   @Override
   public String[] sisterSplitters() {
-    // TODO: no idea if this can/should be improved
+    // TODO: the SR Parser does not use this code path, so it is not implemented
     return EMPTY_SISTERS;
   }
 
   @Override
   public Tree transformTree(Tree t, Tree root) {
-    // TODO
+    // TODO: the SR Parser does not use this code path, so it is not implemented
     return t;
+  }
+
+  public static class HungarianSubcategoryStripper extends TreeNormalizer {
+    @Override
+    public String normalizeNonterminal(String category) {
+      List<String> pieces = StringUtils.split(category, ":");
+      category = pieces.get(0);
+      if (category.equals("PP-locy")) {
+        category = "PP-LOCY";
+      }
+
+      // TODO: maybe some categories should be kept?
+      pieces = StringUtils.split(category, "-");
+      category = pieces.get(0);
+
+      return pieces.get(0);
+    }
+  }
+
+  TreeNormalizer buildNormalizer() {
+    return new HungarianSubcategoryStripper();
   }
 
   /** {@inheritDoc} */
   @Override
   public TreeReaderFactory treeReaderFactory() {
-    return new PennTreeReaderFactory();
+    if (normalizer == null) {
+      normalizer = buildNormalizer();
+    }
+    return new PennTreeReaderFactory(normalizer);
   }
 
 
@@ -114,4 +144,6 @@ public class HungarianTreebankParserParams extends AbstractTreebankParserParams 
     }
     return ret;
   }
+
+  private static final long serialVersionUID = 5652324513L;
 }
